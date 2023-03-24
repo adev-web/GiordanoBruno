614888a8f6665e27a03e4c4efe2c869490d2cb77
Alessandro Javier Olivares Velarde <68888380+adev-web@users.noreply.github.com>
2023-03-24 13:15:26 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 # exit on error
-set -o errexit
+pass_code = 'PASS_DIRS_APP'
+
+rm -r **/__pycache__/
+rm -r **/migrations/
+
+mkdir app/migrations/
+touch app/migrations/__init__.py
+
+mkdir secure/migrations/
+touch secure/migrations/__init__.py
 
 pip install -r requirements.txt
 
